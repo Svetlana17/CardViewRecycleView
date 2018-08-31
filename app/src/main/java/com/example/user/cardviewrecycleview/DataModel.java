952bfe4544d5258bf16e521015b0dc4adcc11a19
Users <lana.chebotareva.95@mail.ru>
2018-08-31 18:07:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/cardviewrecycleview/DataModel.java
@@ -0,0 +1,27 @@
+package com.example.user.cardviewrecycleview;
+
+public class DataModel {
+    String name;
+    String version;
+    int id_;
+    int image;
+
+    public DataModel(String name, String version, int id_, int image){
+        this.name=name;
+        this.version=version;
+        this.id_=id_;
+        this.image=image;
+    }
+    public String getName() {
+        return name;
+    }
+       public String getVersion() {
+        return version;
+    }
+    public int getId_() {
+        return id_;
+    }
+    public int getImage() {
+        return image;
+    }
+}
